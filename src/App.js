469e69da58e0e8699e88ff61bb527caa43276276
KLoGic <klogic@hotmail.co.th>
2023-07-23 19:38:55 +0700

    feat: fill all mockup chart

--- src/App.js
+++ src/App.js
@@ -6,19 +6,43 @@ import PieChart from './components/pieChart';
 function App() {
   return (
     <div className='App'>
-      <div id='row1' className='grid grid-cols-3 gap-1'>
-        <div>
+      <div className='grid grid-cols-5 grid-rows-2 gap-1'>
+        <div className='grid col-span-2 gap-1'>
           <LineChart title='Seasonal Income' />
         </div>
-        <div>
+        <div className='grid col-span-2 gap-1'>
           <BarChart title='Turn Up Ratio' />
         </div>
-      </div>
-      <div id='row2' className='grid grid-cols-3 grid-rows-2 gap-1'>
-        <div className='grid row-span-2'>
-          <LineChart className='row-span-2 h-fit' title='Guests per Contries' />
+        <div id='col3' className='grid col-span-1 row-span-3 gap-1'>
+          <div className='grid items-center justify-center'>
+            <div>
+              <h1>Hotel Name</h1>
+              <span>Powered By Akado v1.3.1</span>
+            </div>
+          </div>
+          <div className='grid items-center justify-center'>
+            <div>
+              <h1>Avg Rating</h1>
+              <h1>8.1</h1>
+            </div>
+          </div>
+          <div className='grid items-center justify-center'>
+            <div>
+              <h1>Avg Monthly$</h1>
+              <h1>$15,000</h1>
+            </div>
+          </div>
+          <div className='grid items-center justify-center'>
+            <div>
+              <h1>ESG (By LSEG)</h1>
+              <h1>$15,000</h1>
+            </div>
+          </div>
         </div>
-        <div className='grid grid-cols-2 row-span-2 gap-1'>
+        <div className='grid col-span-2 gap-1'>
+          <LineChart title='Guests per Contries' />
+        </div>
+        <div className='grid grid-cols-2 col-span-2 row-span-2 gap-1'>
           <PieChart />
           <PieChart />
           <PieChart />
