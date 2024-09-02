0c3e76eb50e200ef7f49693fdc0676df8c153704
Sarin Udompanish <sarin.udompanish2@lseg.com>
2024-09-02 13:43:26 +0700

    feat: add comboChart component

---
+++ src/components/ComboChart.js
@@ -0,0 +1,68 @@
+import Chart from './Chart';
+
+function ComboChart({ data, title, yAxis1Label, yAxis2Label, displayLegend = false }) {
+  const config = {
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          type: 'line',
+          label: '',
+          data: [],
+          yAxisID: 'yAxis1',
+          fill: true, // not fill the area under the line
+        },
+        {
+          type: 'bar',
+          label: '',
+          data: [],
+          yAxisID: 'yAxis2',
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          text: title,
+        },
+        legend: {
+          display: displayLegend,
+        },
+        tooltip: {
+          callbacks: {
+            label: (tooltipItem) => {
+              let rev = tooltipItem.raw;
+              rev = rev.toString().replace(/\B(?=(\d{2})+(?!\d))/g, ',');
+              return `${rev}%`;
+            },
+          },
+        },
+      },
+      scales: {
+        y: {
+          display: false,
+        },
+        yAxis1: {
+          display: true,
+          position: 'left',
+          title: {
+            display: true,
+            text: yAxis1Label,
+          },
+        },
+        yAxis2: {
+          display: true,
+          position: 'right',
+          title: {
+            display: true,
+            text: yAxis2Label,
+          },
+        },
+      },
+    },
+  };
+
+  return <Chart config={config}></Chart>;
+}
+
+export default ComboChart;
